517885dc34a0410b67b5cf0a40f5938fe6f9adf7
cpt_sztos <yautay@gmail.com>
2025-01-30 22:52:21 +0100

    Implement pinia-plugin-persistedstate for state persistence and initialize count from the database

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import piniaPersistedState from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+const pinia = createPinia()
+pinia.use(piniaPersistedState)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
-
 app.mount('#app')
--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,11 +1,13 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import { getCount, setCount } from '@/db'
 
 export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
+  const count = ref(getCount())
   const doubleCount = computed(() => count.value * 2)
   function increment() {
     count.value++
+    setCount(count.value)
   }
 
   return { count, doubleCount, increment }
